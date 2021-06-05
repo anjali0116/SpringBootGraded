0314640a9e83be1f231cab102a29fd69cbe67dca
anjali0116 <anjalimadishetti11@gmail.com>
2021-06-05 21:52:12 +0530

    first commit

---
+++ src/main/java/com/hcl/SpringBootGradedApplication.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradedApplication.class, args);
+	}
+
+}
